de23e2dcae646f4a9461a3e0981e799b1963aa1e
James Ofori Ayerakwa <jamesofoaye@gmail.com>
2021-01-18 13:45:42 +0000

    Added Pinterest tag for tracking ads or visiters from pinterest

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,22 @@ export default function Home() {
           integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
           crossOrigin="anonymous"
         />
+          <!-- Pinterest Tag -->
+          <script>
+              !function(e){if(!window.pintrk){window.pintrk = function () {
+              window.pintrk.queue.push(Array.prototype.slice.call(arguments))};var
+              n=window.pintrk;n.queue=[],n.version="3.0";var
+              t=document.createElement("script");t.async=!0,t.src=e;var
+              r=document.getElementsByTagName("script")[0];
+              r.parentNode.insertBefore(t,r)}}("https://s.pinimg.com/ct/core.js");
+              pintrk('load', '2613497291407', {em: '<user_email_address>'});
+              pintrk('page');
+          </script>
+          <noscript>
+              <img height="1" width="1" style="display:none;" alt=""
+                   src="https://ct.pinterest.com/v3/?event=init&tid=2613497291407&pd[em]=<hashed_email_address>&noscript=1" />
+          </noscript>
+          <!-- end Pinterest Tag -->
       </Head>
 
       <main>
